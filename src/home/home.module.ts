0d5a3be1def7edbe65accf165436b94b2809ba84
Chermann-KING <codes.smiling@gmail.com>
2025-07-04 11:11:26 +0200

    ✨ feat: Ajout de la gestion des langues dans le module et le service Home

--- src/home/home.module.ts
+++ src/home/home.module.ts
@@ -1,15 +1,17 @@
-import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { HomeController } from './home.controller';
-import { HomeService } from './home.service';
-import { Word, WordSchema } from '../dictionary/schemas/word.schema';
-import { User, UserSchema } from '../users/schemas/user.schema';
+import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { HomeController } from "./home.controller";
+import { HomeService } from "./home.service";
+import { Word, WordSchema } from "../dictionary/schemas/word.schema";
+import { User, UserSchema } from "../users/schemas/user.schema";
+import { Language, LanguageSchema } from "../languages/schemas/language.schema";
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Word.name, schema: WordSchema },
       { name: User.name, schema: UserSchema },
+      { name: Language.name, schema: LanguageSchema },
     ]),
   ],
   controllers: [HomeController],
